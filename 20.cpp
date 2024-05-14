4745f9e29c066cb0b5b720eeb518f79216ff2772
Pritanshu69 <samantaprit1924@gmail.com>
2024-05-14 23:41:15 +0530

    20

---
+++ 20.cpp
@@ -0,0 +1,22 @@
+//ANCHOR - valid parenthesis
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+        for (char c : s) { 
+            if (c == '(' || c == '{' || c == '[') { 
+                st.push(c); 
+            } else { 
+                if (st.empty() || 
+                    (c == ')' && st.top() != '(') || 
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; 
+                }
+                st.pop(); 
+            }
+        }
+        return st.empty(); 
+        }
+};
